4bf79f4fb86686f95eada9aff0211593e0cce16a
wandyezj <wandyezj@gmail.com>
2021-10-14 10:17:59 -0700

    basic

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # fluid
 Play With Fluid
+
+Fluid is only for simultaneous sessions. It does not persist data across sessions. Can any client act as a relay?
+
+https://github.com/microsoft/FluidExamples
+
+
+Try to make a turn based game? One Machine needs to be the calculator once all moves are submitted that then updates everyone with the final results. Woo Concurrent programming across the internet.
---
+++ src/app.js
@@ -0,0 +1,91 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { SharedMap } from "fluid-framework";
+import { AzureClient, LOCAL_MODE_TENANT_ID } from "@fluidframework/azure-client";
+import { InsecureTokenProvider } from "@fluidframework/test-client-utils"
+
+// The config is set to run against a local service by default. Run `npx tinylicious` to run locally
+// Update the corresponding properties below with your tenant specific information to run against your tenant.
+const serviceConfig = {
+    connection: {
+        tenantId: LOCAL_MODE_TENANT_ID, // REPLACE WITH YOUR TENANT ID
+        tokenProvider: new InsecureTokenProvider("" /* REPLACE WITH YOUR PRIMARY KEY */, { id: "userId" }),
+        orderer: "http://localhost:7070", // REPLACE WITH YOUR ORDERER ENDPOINT
+        storage: "http://localhost:7070", // REPLACE WITH YOUR STORAGE ENDPOINT
+    }
+};
+
+const client = new AzureClient(serviceConfig);
+
+const diceValueKey = "dice-value-key";
+
+
+const containerSchema = {
+    initialObjects: { diceMap: SharedMap }
+};
+const root = document.getElementById("content");
+
+const createNewDice = async () => {
+    const { container } = await client.createContainer(containerSchema);
+    container.initialObjects.diceMap.set(diceValueKey, 1);
+    const id = await container.attach();
+    renderDiceRoller(container.initialObjects.diceMap, root);
+    return id;
+}
+
+const loadExistingDice = async (id) => {
+    const { container } = await client.getContainer(id, containerSchema);
+    renderDiceRoller(container.initialObjects.diceMap, root);
+}
+
+async function start() {
+    if (location.hash) {
+        await loadExistingDice(location.hash.substring(1))
+    } else {
+        const id = await createNewDice();
+        location.hash = id;
+    }
+}
+
+start().catch((error) => console.error(error));
+
+// Define the view
+
+const template = document.createElement("template");
+
+template.innerHTML = `
+  <style>
+    .wrapper { text-align: center }
+    .dice { font-size: 200px }
+    .roll { font-size: 50px;}
+  </style>
+  <div class="wrapper">
+    <div class="dice"></div>
+    <button class="roll"> Roll </button>
+  </div>
+`
+
+const renderDiceRoller = (diceMap, elem) => {
+    elem.appendChild(template.content.cloneNode(true));
+
+    const rollButton = elem.querySelector(".roll");
+    const dice = elem.querySelector(".dice");
+
+    // Set the value at our dataKey with a random number between 1 and 6.
+    rollButton.onclick = () => diceMap.set(diceValueKey, Math.floor(Math.random() * 6) + 1);
+
+    // Get the current value of the shared data to update the view whenever it changes.
+    const updateDice = () => {
+        const diceValue = diceMap.get(diceValueKey);
+        // Unicode 0x2680-0x2685 are the sides of a dice (⚀⚁⚂⚃⚄⚅)
+        dice.textContent = String.fromCodePoint(0x267f + diceValue);
+        dice.style.color = `hsl(${diceValue * 60}, 70%, 30%)`;
+    };
+    updateDice();
+
+    // Use the changed event to trigger the rerender whenever the value changes.
+    diceMap.on("valueChanged", updateDice);
+}
